0ed31670648ba8f50138917528a35dae08157170
ghalibse <ghalibse@gmail.com>
2016-08-16 15:23:28 -0400

    First Commit

---
+++ app/src/main/java/com/example/simpleunittest/CalculatorHelper.java
@@ -0,0 +1,22 @@
+package com.example.simpleunittest;
+
+
+public class CalculatorHelper {
+
+
+    public int sumItems(int n1, int n2) {
+
+        return n1 + n2;
+    }
+
+    public int multiplyItems(int n1, int n2) {
+
+        return n1 * n2;
+    }
+
+    public int powerItems(int n1, int n2) {
+
+
+        return (int) Math.pow(n1, n2);
+    }
+}
